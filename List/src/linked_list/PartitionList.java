0a0031de934778aa3b469c208f400663c666bd16
九渊 <1058477702@qq.com>
2024-08-19 01:00:52 +0800

    feat: 分隔链表

---
+++ List/src/linked_list/PartitionList.java
@@ -0,0 +1,30 @@
+package linked_list;
+
+import data_structure.ListNode;
+
+public class PartitionList {
+    // 分隔链表
+    // 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+    // 你应当 保留 两个分区中每个节点的初始相对位置。
+    // 虚拟节点 + 双指针
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallDummy = new ListNode(0);
+        ListNode largeDummy = new ListNode(0);
+        ListNode small = smallDummy;
+        ListNode large = largeDummy;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null; // 断开原有链接
+        small.next = largeDummy.next; // 拼接两个链表
+        return smallDummy.next;
+    }
+
+}
