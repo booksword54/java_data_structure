9d6ef0f584f451fedf92bc8399e0c5ff765d182a
九渊 <1058477702@qq.com>
2024-08-18 12:09:59 +0800

    update: 生命游戏

--- Array/src/matrix/GameOfLife.java
+++ Array/src/matrix/GameOfLife.java
@@ -11,11 +11,22 @@ public class GameOfLife {
     // - 如果死细胞周围正好有三个活细胞，则该位置死细胞复活；
     // 下一个状态是通过将上述规则同时应用于当前状态下的每个细胞所形成的，其中细胞的出生和死亡是同时发生的。
     // 给你 m x n 网格面板 board 的当前状态，返回下一个状态。
+    private static final Integer DEAD = 0;
+    private static final Integer LIVE = 1;
 
     public void gameOfLife(int[][] board) {
         int[] neighbors = {0, 1, -1};
         int rows = board.length;
         int cols = board[0].length;
+
+        int[][] copyBoard = new int[rows][cols];
+        // 拷贝当前细胞存活状态的数组，细胞的生存当前状态，board是下一局的状态
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+        // 遍历 copyBoard 判断细胞的生存
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int liveNeighbors = 0; // 相邻位置存活细胞的数量
@@ -24,7 +35,7 @@ public class GameOfLife {
                         if (!(neighbors[i] == 0 && neighbors[j] == 0)) { // 查看相邻元素， (0,0)是自己
                             int r = row + neighbors[i];
                             int c = col + neighbors[j];
-                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == LIVE)) {
                                 liveNeighbors += 1; // 相邻存活细胞加一
                             }
                         }
@@ -32,26 +43,16 @@ public class GameOfLife {
                 }
                 // 如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡；
                 // 如果活细胞周围八个位置有超过三个活细胞，则该位置活细胞死亡；
-                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
-                    board[row][col] = -1; // 下局才会死亡，本局仍然存活，绝对值是1
+                if ((board[row][col] == LIVE) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = DEAD; // 下局死亡
                 }
                 // 如果活细胞周围八个位置有两个或三个活细胞，则该位置活细胞仍然存活；
-                if ((board[row][col] == 1) && (liveNeighbors == 2 || liveNeighbors == 3)) {
-                    board[row][col] = 1; // 下局存活，本局也存活，绝对值是1
+                if ((board[row][col] == LIVE) && (liveNeighbors == 2 || liveNeighbors == 3)) {
+                    board[row][col] = LIVE; // 下局存活
                 }
                 // 如果死细胞周围正好有三个活细胞，则该位置死细胞复活；
-                if (board[row][col] == 0 && liveNeighbors == 3) {
-                    board[row][col] = 2; // 下局才会复活，，本局仍然死亡
-                }
-            }
-        }
-        // 更新细胞状态
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                if (board[row][col] > 0) { // 最新状态不是 -1 和 0
-                    board[row][col] = 1;
-                } else {
-                    board[row][col] = 0;
+                if (board[row][col] == DEAD && liveNeighbors == 3) {
+                    board[row][col] = LIVE; // 下局复活
                 }
             }
         }
