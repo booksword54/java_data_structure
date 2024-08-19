592643680b764cd9275980cd0202997a4fac958e
九渊 <1058477702@qq.com>
2024-08-19 22:47:58 +0800

    update: 从前序与中序遍历序列构造二叉树

--- Tree/src/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ Tree/src/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -7,6 +7,7 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
     // 从前序与中序遍历序列构造二叉树
     // 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
 
+    // 存放中序遍历的节点及其下标，便于获取子树大小 inRoot-inLeft
     Map<Integer, Integer> indexMap = new HashMap<>();
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -18,13 +19,18 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
     }
 
     private TreeNode helper(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRight) {
-        if (preLeft > preRight) { // 不构成树
+        if (preLeft > preRight) { // 节点为空
             return null;
         }
-        int inRoot = indexMap.get(preorder[preLeft]);
+        // 根节点
         TreeNode root = new TreeNode(preorder[preLeft]);
+        // 根节点位置
+        int inRoot = indexMap.get(preorder[preLeft]);
+        // 左子树大小
         int sizeLeft = inRoot - inLeft;
+        // 构造左子树，preLeft是根节点，[preLeft+1, preLeft+sizeLeft] [inLeft, inRoot - 1]是左子树节点范围
         root.left = helper(preorder, inorder, preLeft + 1, preLeft + sizeLeft, inLeft, inRoot - 1);
+        // 构造右子树，preLeft是根节点，[preLeft+sizeLeft+1, preRight] [inRoot+1, inRight]是右子树节点范围，
         root.right = helper(preorder, inorder, preLeft + sizeLeft + 1, preRight, inRoot + 1, inRight);
         return root;
     }
