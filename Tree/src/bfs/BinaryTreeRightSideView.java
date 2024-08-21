ae88eb35754c0f060715d575ecb9baa551913281
九渊 <1058477702@qq.com>
2024-08-21 19:29:26 +0800

    feat: 二叉树的右视图

---
+++ Tree/src/bfs/BinaryTreeRightSideView.java
@@ -0,0 +1,39 @@
+package bfs;
+
+import data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+    // 二叉树的右视图
+    // 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            // 一次遍历一层节点
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                // 每层节点的最后一个，即最右节点
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
