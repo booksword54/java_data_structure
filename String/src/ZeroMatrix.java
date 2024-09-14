30cb6dc6cec7ca87a151a96d9eff33de789f0256
九渊 <1058477702@qq.com>
2024-09-14 21:20:42 +0800

    feat: 零矩阵

---
+++ String/src/ZeroMatrix.java
@@ -0,0 +1,77 @@
+public class ZeroMatrix {
+    // 编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
+    // 使用标记数组
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 记录该行该列设置成0
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // 使用两个标记变量
+    // 原理是使用第一行、列作为全为零的标记数组，两个标记变量的作用是最后处理第一行列成零
+    public void setZeroes2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        // 标记第一列为零
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        // 标记第一行为零
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        // 记录标记数组
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    // 记录对应第一列、行为0的标记
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        // 标记数组生效
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // 第一列设置为零
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        // 第一行设置为零
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
